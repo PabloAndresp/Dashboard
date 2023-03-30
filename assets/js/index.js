06b52bad12753dbefa10ff7d3cb39546423a54b6
Pablo <Pabloandr@live.cl>
2023-03-30 19:41:41 -0300

    cambio de color de la fuente de chartjs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 import { createChart } from './charts.js';
 import { getCoronavirusData } from './coronavirus-api.js';
 
+Chart.defaults.color = '#fff';
+
 const getDataColor = opacity => {
   const colors = ['#7448c2', '#21c0d7', '#d99e2b', '#cd3a81', '#9c99cc', '#e14eca', '#ffffff', '#ff0000', '#d6ff00', '#0038ff'];
   return colors.map(color => opacity ? `${color + opacity}` : color)
